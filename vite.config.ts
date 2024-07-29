bada31ae58a8b2b8e3e3c19b7211b5e2cb928006
Gennady Beryukaev <gberyukaev@a2seven.com>
2024-07-29 09:50:50 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/foo': 'http://localooooooo:4567',
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+});
