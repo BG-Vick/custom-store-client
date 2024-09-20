c6cf85532b700b3cc6c0d2a2ae03f97f4576717c
Gennady Beryukaev <gberyukaev@ibs.ru>
2024-09-20 14:25:07 +0300

    настройка инфраструктуры

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,46 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
+  /*server: {
     proxy: {
-      '/foo': 'http://localooooooo:4567',
       '/api': {
+        target: 'https://dummyjson.com',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        changeOrigin: true,
+      },
+      '/foo': {
         target: 'http://jsonplaceholder.typicode.com',
+        rewrite: (path) => path.replace(/^\/foo/, ''),
         changeOrigin: true,
         secure: false,
       },
     },
+  },*/
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  plugins: [react()],
+  //envPrefix: 'APP_',
 });
